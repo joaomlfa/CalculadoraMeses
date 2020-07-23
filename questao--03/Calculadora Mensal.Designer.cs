ee6b8c34947e549084f751047f17004ebe72d7e2
Joao Moreira <joaomlfa@hotmail.com>
2020-07-22 23:29:11 -0300

    Calculadora de Meses para processo seletivo de desenvolvedor no SuasVendas.

---
+++ questao--03/Calculadora Mensal.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace questao__03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textSalario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textEconomizar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textValorApartamento = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Salário Anual";
+            // 
+            // textSalario
+            // 
+            this.textSalario.Location = new System.Drawing.Point(123, 77);
+            this.textSalario.Name = "textSalario";
+            this.textSalario.Size = new System.Drawing.Size(100, 22);
+            this.textSalario.TabIndex = 1;
+            this.textSalario.TextChanged += new System.EventHandler(this.textSalario_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "% A Economizar Mensalmente";
+            // 
+            // textEconomizar
+            // 
+            this.textEconomizar.Location = new System.Drawing.Point(123, 166);
+            this.textEconomizar.Name = "textEconomizar";
+            this.textEconomizar.Size = new System.Drawing.Size(100, 22);
+            this.textEconomizar.TabIndex = 3;
+            this.textEconomizar.TextChanged += new System.EventHandler(this.textEconomizar_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Valor do Apartamento";
+            // 
+            // textValorApartamento
+            // 
+            this.textValorApartamento.Location = new System.Drawing.Point(123, 260);
+            this.textValorApartamento.Name = "textValorApartamento";
+            this.textValorApartamento.Size = new System.Drawing.Size(100, 22);
+            this.textValorApartamento.TabIndex = 5;
+            this.textValorApartamento.TextChanged += new System.EventHandler(this.textValorApartamento_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 50);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calcular Meses";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(250, 476);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 474);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textValorApartamento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textEconomizar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textSalario);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Meses";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textSalario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textEconomizar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textValorApartamento;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ questao--03/Calculadora Mensal.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace questao__03
+{
+    public partial class Form1 : Form
+    {        
+        private const decimal porcentagem_entrada = 0.25M;
+        private const decimal r = 0.04M;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           //Verificar se os campos estão Digitados.
+            if (textSalario.Text.Equals(""))
+            {
+                MessageBox.Show("Digite o valor do Salário.");
+                textSalario.Focus();
+                return;
+            }else if (textEconomizar.Text.Equals(""))
+            {
+                MessageBox.Show("Digite o percentual de economia Mensal.");
+                textEconomizar.Focus();
+                return;
+            }
+            else if (textValorApartamento.Text.Equals(""))
+            {
+                MessageBox.Show("Digite o valor do Apartamento.");
+                textValorApartamento.Focus();
+                return;
+            }
+
+            //Declaração de Variáveis
+            int quantidade_meses_necessario = 0;
+            decimal salario_anual;
+            decimal salario_mensal;
+            decimal valor_entrada;
+            decimal economias_atuais = 0;
+            decimal percentual_economizado;
+            decimal custo_total;
+
+            //Atribuir os campos a suas Respectivas Variáveis.
+
+            salario_anual = Convert.ToDecimal(textSalario.Text.Replace(",",".")) ;
+            percentual_economizado = Convert.ToDecimal(textEconomizar.Text.Replace(",", ".")) /100;
+            custo_total = Convert.ToDecimal(textValorApartamento.Text.Replace(",", "."));
+
+            if(percentual_economizado > 1)
+            {
+                MessageBox.Show("Percentual Economizado mensalmente deve ser menor que 100%");
+                textEconomizar.Focus();
+                return;
+            }
+
+            //Calcular salario Mensal.
+            salario_mensal = salario_anual / 12;
+
+            //Calcular o valor da entrada do Apartamento.
+            valor_entrada = custo_total * porcentagem_entrada;
+
+            //Calcular meses necessários.
+            for(int i = 1; economias_atuais < valor_entrada; i++)
+            {
+                
+                decimal retorno;
+
+                // Verifica se a economia Atual é 0 ou se possui valor para calcular  retorno anual.
+                if(economias_atuais == 0)
+                {
+                    retorno = 0;
+                }
+                else
+                {
+                    retorno = economias_atuais * r / 12;
+                    economias_atuais += retorno;
+                }
+                //Calcular percentual mensal do salário
+                economias_atuais += salario_mensal * percentual_economizado;
+
+                //Acrescentar um mes na quantidade necessária.
+                quantidade_meses_necessario++;
+            }
+            //Exibe a quantidade de mês necessário.
+            MessageBox.Show("Serão Necessários " + quantidade_meses_necessario.ToString() + " Meses de Economia");
+        }
+
+
+
+
+        // Permitir apenas Números e Vírgula/Ponto
+        private void textSalario_TextChanged(object sender, EventArgs e)
+        {
+            textSalario.Text = Regex.Replace(textSalario.Text, "[^0-9,.]", "");
+        }
+
+        private void textEconomizar_TextChanged(object sender, EventArgs e)
+        {
+            textSalario.Text = Regex.Replace(textSalario.Text, "[^0-9,.]", "");
+        }
+
+        private void textValorApartamento_TextChanged(object sender, EventArgs e)
+        {
+            textSalario.Text = Regex.Replace(textSalario.Text, "[^0-9,.]", "");
+        }
+    }
+}
